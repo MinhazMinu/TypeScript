6c7f4a394d188fea55a6791ed8b6882ed4e391f9
Minhaz <minhazul.taher@gmail.com>
2023-04-25 10:18:56 +0600

    Chapter 02 : Basic Types complited

---
+++ Lesson2/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+let myName = "minhaz";
+let age = 21;
+let isDeveloper = true;
+let anything = "anything";
+let postId;
+let isActive;
+let regExp = /minhaz/;
---
+++ Lesson2/src/main.ts
@@ -0,0 +1,7 @@
+let myName: string = "minhaz";
+let age: number = 21;
+let isDeveloper: boolean = true;
+let anything: any = "anything";
+let postId: string | number;
+let isActive: boolean | number;
+let regExp: RegExp = /minhaz/;
